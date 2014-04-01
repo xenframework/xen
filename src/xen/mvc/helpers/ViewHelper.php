954cdbf94df8b4bf4e0229598fa599aab3caca2c
itrascastro <i.trascastro@gmail.com>
2014-04-01 10:43:00 +0200

    phpdoc

--- src/xen/mvc/helpers/ViewHelper.php
+++ src/xen/mvc/helpers/ViewHelper.php
@@ -16,12 +16,43 @@
 
 namespace xen\mvc\helpers;
 
+/**
+ * Class ViewHelper
+ *
+ * ViewHelper Abstraction
+ *
+ * @package    xenframework
+ * @subpackage xen\mvc\helpers
+ * @author     Ismael Trascastro <itrascastro@xenframework.com>
+ * @copyright  Copyright (c) xenFramework. (http://xenframework.com)
+ * @license    MIT License - http://en.wikipedia.org/wiki/MIT_License
+ * @link       https://github.com/xenframework/xen
+ * @since      Class available since Release 1.0.0
+ */
 abstract class ViewHelper
 {
+    /**
+     * @var string The ViewHelper html generated
+     */
     protected $_html;
 
+    /**
+     * __construct
+     *
+     * The ViewHelper constructor
+     * It can receive params from the View
+     *
+     * @param array $params
+     */
     abstract function __construct($params=array());
 
+    /**
+     * show
+     *
+     * Method used for retrieve the ViewHelper html
+     *
+     * @return string
+     */
     public function show()
     {
         return $this->_html;
